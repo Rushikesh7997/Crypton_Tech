19a8583cc13639c8cfe4c96f9bbc1fcfc74783ff
Rushikesh7997 <pardhikar26@gmail.com>
2023-04-05 19:02:38 +0530

    api folder strutre update

--- orderstatsapp/src/App.tsx
+++ orderstatsapp/src/App.tsx
@@ -1,13 +1,15 @@
-import { useEffect, useState } from 'react';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState([{}]);
 
   const OrderStatsApp = async () => {
     try {
       const res = await fetch("http://localhost:3500/data");
       const data = await res.json();
       console.log(data);
+      setData(data);
     } catch (error) {
       console.log(error);
     }
@@ -18,8 +20,12 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      
+    <div className="container">
+      {data.map((data) => {
+        return <div>
+          <p></p>
+        </div>;
+      })}
     </div>
   );
 }
